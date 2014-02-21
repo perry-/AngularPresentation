3b41c6d1a1bce7982e7d2d5e0dc27d175b0000a3
Alexander Perry <yrrepxela@gmail.com>
2014-02-21 15:17:19 +0100

    Minor changes

--- js/app.js
+++ js/app.js
@@ -1,35 +1,17 @@
 var presentation = angular.module('presentation', []);
 
 presentation.controller('bindingCtrl', function($scope) {
-  $scope.title = "Two Way Binding";
+	$scope.title = "Two Way Binding";
 });
 
-presentation.controller('forEachCtrl', function($scope) {
-	$scope.title = "ngRepeat";
-
-	$scope.orders = [
-        {'OrderId': 5200902, 'OrderName': 'Physical treatment'},
-		{'OrderId': 5200903, 'OrderName': 'Dentist appointment'},
-		{'OrderId': 5200904, 'OrderName': 'Sick children'}
-	];
-	
+presentation.controller('ngRepeatCtrl', function($scope) {
+	$scope.title = "ng-Repeat";
 });
 
 presentation.controller('filterCtrl', function($scope) {
-  $scope.title = "Filter";
+	$scope.title = "Filter";
 }); 
 
-presentation.controller('localeCtrl', function($scope, $locale) {
-	$scope.background = "Norway.svg";
-	$scope.title = "Internasjonalisering";
-
-	if ($locale.id == 'en') {
-		$scope.background = "English.png";
-		$scope.title = "Internationalization";
-	}
-	else if($locale.id == 'de') {
-		$scope.background = "Deutsch.png";
-		$scope.title = "Internationalisierung";
-	}
-	
+presentation.controller('localeCtrl', function($scope) {
+	$scope.title = "";
 });
